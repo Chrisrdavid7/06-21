0d23c47ea8bc3d9a48df59c8d4a8e76b42216310
Chrisrdavid7 <chrisrdavid7@gmail.com>
2015-06-21 11:40:53 -0400

    Set-Up Outlines for Patron and Bank Account

--- 06-21/main.m
+++ 06-21/main.m
@@ -8,11 +8,56 @@
 
 #import <Foundation/Foundation.h>
 
+@interface Patron : NSObject
+- (void)pocket:(int)p;
+- (void)with:(int)w;
+- (void)depo:(int)d;
+@end
+
+@implementation Patron {
+    int pocket;
+    int withdrawn;
+    int deposit;
+}
+- (void) pocket:(int) p{
+    pocket = p;
+}
+- (void)with: (int) w{
+    withdrawn = w;
+}
+- (void)depo: (int) d{
+    deposit = d;
+}
+
+@end
+
+
+@interface BankAccount : NSObject
+- (void)balance:(int)n;
+
+@end
+
+@implementation BankAccount {
+int accbalance;
+    int withdrawn;
+}
+- (void)balance: (int) n {
+    accbalance = n;
+}
+- (void)trans: (int) w{
+    withdrawn = w;
+}
+@end
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
     
-
+        BankAccount *testsAccount =[[BankAccount alloc] init];
+        [testsAccount balance:2];
+        
+        
       printf("Did it work?");  
+        
         
         
     }
